a141e11a60051c0981581324343ecec249c90125
David <83354685+DavidLopez888@users.noreply.github.com>
2024-03-16 00:49:07 +0100

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname));
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
