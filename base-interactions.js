cc850701a2c0259f526181ed19616eb0ef438d44
David <83354685+DavidLopez888@users.noreply.github.com>
2024-04-04 00:49:42 +0200

    03042024N

--- base-interactions.js
+++ base-interactions.js
@@ -14,7 +14,7 @@ async function obtenerDiferenciaHorariaUsuario() {
     const ubicacionUsuario = await obtenerUbicacionUsuario();
     if (ubicacionUsuario) {
       const zonaHorariaUsuario = Intl.DateTimeFormat().resolvedOptions().timeZone;
-      const diferenciaHoraria = obtenerDiferenciaHoraria("Etc/GMT+5", zonaHorariaUsuario);      
+      const diferenciaHoraria = obtenerDiferenciaHoraria("Etc/GMT+5", zonaHorariaUsuario);
       return diferenciaHoraria;
     } else {
       console.warn("La geolocalizacion no esta disponible o no se ha proporcionado permiso.");
@@ -99,31 +99,31 @@ const fetchData = async () => {
 
     const eventosContainer = document.getElementById('eventos-container');
     const diferenciaHorariaUsuario = await obtenerDiferenciaHorariaUsuario();
-    
+
     // Limpiar el contenido actual del contenedor
     eventosContainer.innerHTML = '';
 
     const eventosOrdenados = result.Items ? result.Items.filter(item => {
       return typeof item.f04_hora_event.S === 'string';}).sort((a, b) => {
-        return a.f04_hora_event.S.localeCompare(b.f04_hora_event.S);}) : [];    
-    
-    eventosOrdenados.forEach((doc) => {      
+        return a.f04_hora_event.S.localeCompare(b.f04_hora_event.S);}) : [];
+
+    eventosOrdenados.forEach((doc) => {
       const data = doc;
       const horaAjustada = ajustarHoraEvento(data.f04_hora_event, diferenciaHorariaUsuario);
-      
+
       // Calcular la diferencia de horas entre la hora de ejecucion del usuario y la hora ajustada del evento
       const diferenciaHoras = calcularDiferenciaHoras(horaEjecucionUsuario, horaAjustada);
       const proveedor = typeof data.f02_proveedor === 'object' ? data.f02_proveedor.S : data.f02_proveedor;
-      if ((diferenciaHoras >= -120 && diferenciaHoras <= 30) || (typeof proveedor === 'string' && proveedor.includes("LiveTV"))) {
+      if ((diferenciaHoras >= -120 && diferenciaHoras <= 15) || (typeof proveedor === 'string' && proveedor.includes("LiveTV"))) {
         // Crear un elemento div para cada evento
         const eventoDiv = document.createElement('div');
         eventoDiv.classList.add('evento');
-    
+
         // Verificar si f07_URL_Flag no es null
         const eventoHeader = document.createElement('div');
         eventoHeader.classList.add('evento-header');
         eventoDiv.appendChild(eventoHeader);
-    
+
         // Crear un contenedor para banderaImg, horaEvento y categoryEvento
         const infoEventoContainer = document.createElement('div');
         infoEventoContainer.classList.add('info-evento-container');
@@ -135,11 +135,6 @@ const fetchData = async () => {
             banderaImg.classList.add('bandera');
             infoEventoContainer.appendChild(banderaImg);
         }
-
-        const horaEvento = document.createElement('p');
-        horaEvento.textContent = `${horaAjustada} `; 
-        horaEvento.classList.add('hora-evento');
-        infoEventoContainer.appendChild(horaEvento);
 
         const categoryEvento = document.createElement('p');
         categoryEvento.textContent = `| ${data.f05_event_categoria && typeof data.f05_event_categoria === 'object' && data.f05_event_categoria.hasOwnProperty('S') ? data.f05_event_categoria.S : ''} `;
@@ -153,41 +148,86 @@ const fetchData = async () => {
         // Crear un contenedor para textoEvento y las imagenes
         const textoImagenesContainer = document.createElement('div');
         textoImagenesContainer.classList.add('texto-imagenes-container');
-        textoImagenesContainer.style.display = 'flex';
-        textoImagenesContainer.style.alignItems = 'center';
-
-        // Cargar la imagen de f09_logo_Local antes de f06_name_event
-        if (data.f09_logo_Local !== null && typeof data.f09_logo_Local === 'object' && data.f09_logo_Local.hasOwnProperty('S')) {
-          const logoLocalImg = document.createElement('img');
-          logoLocalImg.src = data.f09_logo_Local.S;
-          logoLocalImg.alt = 'Logo Local';
-          logoLocalImg.classList.add('logo-local');
-          logoLocalImg.style.width = '40px'; // Ajusta el ancho segun sea necesario
-          logoLocalImg.style.height = 'auto'; // Manten la proporcion original          
-          logoLocalImg.style.marginRight = '10px'; // Ajusta el margen derecho segun sea necesario
-          textoImagenesContainer.appendChild(logoLocalImg);
-        }
+        //textoImagenesContainer.style.display = 'flex';
+        //textoImagenesContainer.style.justifyContent = 'center'; // Centra horizontalmente los elementos
+        //textoImagenesContainer.style.alignItems = 'center';
 
         const textoEvento = document.createElement('p');
         textoEvento.textContent = `${data.f06_name_event && typeof data.f06_name_event === 'object' && data.f06_name_event.hasOwnProperty('S') ? data.f06_name_event.S : ''}`;
         textoEvento.classList.add('texto-evento');
-        textoImagenesContainer.appendChild(textoEvento);
+        // textoImagenesContainer.appendChild(textoEvento);
 
-        // Cargar la imagen de f11_logo_Visita despues de f06_name_event
-        if (data.f11_logo_Visita !== null && typeof data.f11_logo_Visita === 'object' && data.f11_logo_Visita.hasOwnProperty('S')) {
-          const logoVisitaImg = document.createElement('img');
-          logoVisitaImg.src = data.f11_logo_Visita.S;
-          logoVisitaImg.alt = 'Logo Visita';
-          logoVisitaImg.classList.add('logo-visita');
-          logoVisitaImg.style.width = '40px'; // Ajusta el ancho segun sea necesario
-          logoVisitaImg.style.height = 'auto'; // Manten la proporcion original          
-          logoVisitaImg.style.marginLeft = '10px'; // Ajusta el margen izquierdo segun sea necesario
-          textoImagenesContainer.appendChild(logoVisitaImg);
+        // Verificar si textoEvento contiene "vs" (mayusculas o minusculas)
+        const textoEventoString = textoEvento.textContent;
+        const vsIndex = textoEventoString.toLowerCase().indexOf(' vs ');
+        // console.log("textoEventoString.toLowerCase(): ", textoEventoString.toLowerCase());
+        // console.log("vsIndex: ", vsIndex);
+        if (vsIndex !== -1) {
+          // Dividir el texto en dos partes antes y despues de "vs"
+          const textoEventoIzquierda_vs = textoEventoString.slice(0, vsIndex).trim(); 
+          const textoEventoDerecha_vs = textoEventoString.slice(vsIndex + 3).trim();
+
+          // Crear elementos para cada parte del texto
+          const textoEventoIzquierdaElement = document.createElement('p');
+          textoEventoIzquierdaElement.textContent = textoEventoIzquierda_vs;
+          textoEventoIzquierdaElement.classList.add('texto-evento-izquierda');
+          textoImagenesContainer.appendChild(textoEventoIzquierdaElement);
+
+          const horaEvento = document.createElement('p');
+          horaEvento.textContent = `${horaAjustada} `;
+          horaEvento.style.width = 'fit-content'; // Ajusta el ancho del contenedor al contenido
+          horaEvento.style.margin = 'auto'; // Centra el contenedor horizontalmente          
+          horaEvento.classList.add('hora-evento');
+          textoImagenesContainer.appendChild(horaEvento);
+
+          const textoEventoDerechaElement = document.createElement('p');
+          textoEventoDerechaElement.textContent = textoEventoDerecha_vs;
+          textoEventoDerechaElement.classList.add('texto-evento-derecha');      
+          textoImagenesContainer.appendChild(textoEventoDerechaElement);
+
+          // Agregar logotipos a ambos lados de "vs"
+          // Cargar la imagen de f09_logo_Local antes de f06_name_event
+          if (data.f09_logo_Local !== null && typeof data.f09_logo_Local === 'object' && data.f09_logo_Local.hasOwnProperty('S')) {
+            const logoLocalImg = document.createElement('img');
+            logoLocalImg.src = data.f09_logo_Local.S;
+            logoLocalImg.alt = 'Logo Local';
+            logoLocalImg.classList.add('logo-local');
+            logoLocalImg.style.width = '40px'; // Ajusta el ancho según sea necesario
+            logoLocalImg.style.height = 'auto'; // Mantén la proporción original
+            logoLocalImg.style.marginRight = '10px'; // Ajusta el margen derecho según sea necesario
+            logoLocalImg.classList.add('logo-evento-local');    
+            textoImagenesContainer.appendChild(logoLocalImg);    
+            textoImagenesContainer.insertBefore(logoLocalImg, textoEventoIzquierdaElement.nextSibling);
+          }
+
+          // Cargar la imagen de f11_logo_Visita después de f06_name_event
+          if (data.f11_logo_Visita !== null && typeof data.f11_logo_Visita === 'object' && data.f11_logo_Visita.hasOwnProperty('S')) {
+            const logoVisitaImg = document.createElement('img');
+            logoVisitaImg.src = data.f11_logo_Visita.S;
+            logoVisitaImg.alt = 'Logo Visita';
+            logoVisitaImg.classList.add('logo-visita');
+            logoVisitaImg.style.width = '40px'; // Ajusta el ancho según sea necesario
+            logoVisitaImg.style.height = 'auto'; // Mantén la proporción original
+            logoVisitaImg.style.marginLeft = '10px'; // Ajusta el margen izquierdo según sea necesario
+            logoVisitaImg.classList.add('logo-evento-visita'); 
+            textoImagenesContainer.appendChild(logoVisitaImg);     
+            textoImagenesContainer.insertBefore(logoVisitaImg, textoEventoDerechaElement);
+          }
+        } else {
+          const horaEvento = document.createElement('p');
+          horaEvento.textContent = `${horaAjustada} `;
+          horaEvento.style.width = 'fit-content'; // Ajusta el ancho del contenedor al contenido
+          horaEvento.style.margin = 'auto'; // Centra el contenedor horizontalmente          
+          horaEvento.classList.add('hora-evento');
+
+          textoImagenesContainer.appendChild(horaEvento);
+
+          // Si no contiene "vs", simplemente agregamos el texto original
+          textoEvento.classList.add('texto-evento-izquierda');
+          textoImagenesContainer.appendChild(textoEvento);
         }
-
         // Agregar el contenedor de textoEventoContainer al eventoHeader
         eventoHeader.appendChild(textoImagenesContainer);
-
 
         // Esta parte hace clickeable el header para mostrar/ocultar detalles
         eventoHeader.addEventListener('click', () => {
@@ -196,14 +236,14 @@ const fetchData = async () => {
           if (detalle) {
               detalle.style.display = detalle.style.display === 'none' ? 'block' : 'none';
           }
-        });         
-        
+        });
+
         // Verifica si data.f20_Detalles_Evento es un objeto
         if (typeof data.f20_Detalles_Evento === 'object' && data.f20_Detalles_Evento !== null) {
             const detalleEventoContainer = document.createElement('div'); // Crear un contenedor para los detalles del evento
             detalleEventoContainer.classList.add('detalle-evento-container');
             detalleEventoContainer.style.display = 'none'; // Ocultar por defecto
-    
+
             const eventoDetalle = document.createElement('ul');
             eventoDetalle.classList.add('detalle-evento');
             data.f20_Detalles_Evento.L.forEach(detalle => {
@@ -239,10 +279,10 @@ const fetchData = async () => {
         } else {
             console.error("data.f20_Detalles_Evento no es un objeto o es nulo.");
         }
-    
+
         // Agregar el elemento del evento al contenedor principal
         eventosContainer.appendChild(eventoDiv);
-      }    
+      }
     });
 
     console.log("Conexion exitosa. Datos recuperados correctamente 3.");
@@ -275,18 +315,18 @@ const fetchData = async () => {
 //     // Ejecutar la consulta a DynamoDB
 //     const result = await dynamodb.query(params).promise();
 //     const eventosContainer = document.getElementById('eventos-container');
-    
+
 //     // Limpiar el contenido actual del contenedor
 //     eventosContainer.innerHTML = '';
 
 //     const eventosOrdenados = result.Items ? result.Items.filter(item => {
 //       return typeof item.f04_hora_event.S === 'string';}).sort((a, b) => {
-//         return a.f04_hora_event.S.localeCompare(b.f04_hora_event.S);}) : [];    
+//         return a.f04_hora_event.S.localeCompare(b.f04_hora_event.S);}) : [];
 
-//     eventosOrdenados.forEach((doc) => {      
+//     eventosOrdenados.forEach((doc) => {
 //       const data = doc;
 //       const horaAjustada = ajustarHoraEvento(data.f04_hora_event, diferenciaHorariaUsuario);
-      
+
 //       // Calcular la diferencia de horas entre la hora de ejecucion del usuario y la hora ajustada del evento
 //       const diferenciaHoras = calcularDiferenciaHoras(horaEjecucionUsuario, horaAjustada);
 //       const proveedor = typeof data.f02_proveedor === 'object' ? data.f02_proveedor.S : data.f02_proveedor;
@@ -294,12 +334,12 @@ const fetchData = async () => {
 //         // Crear un elemento div para cada evento
 //         const eventoDiv = document.createElement('div');
 //         eventoDiv.classList.add('evento');
-    
+
 //         // Verificar si f07_URL_Flag no es null
 //         const eventoHeader = document.createElement('div');
 //         eventoHeader.classList.add('evento-header');
 //         eventoDiv.appendChild(eventoHeader);
-    
+
 //         if (data.f07_URL_Flag !== null && typeof data.f07_URL_Flag === 'object' && data.f07_URL_Flag.hasOwnProperty('S')) {
 //             const banderaImg = document.createElement('img');
 //             banderaImg.src = data.f07_URL_Flag.S;
@@ -307,17 +347,17 @@ const fetchData = async () => {
 //             banderaImg.classList.add('bandera');
 //             eventoHeader.appendChild(banderaImg);
 //         }
-    
+
 //         const textoEvento = document.createElement('p');
 //         textoEvento.textContent = `${horaAjustada} | ${data.f05_event_categoria && typeof data.f05_event_categoria === 'object' && data.f05_event_categoria.hasOwnProperty('S') ? data.f05_event_categoria.S : ''} | ${data.f06_name_event && typeof data.f06_name_event === 'object' && data.f06_name_event.hasOwnProperty('S') ? data.f06_name_event.S : ''}`;
 //         textoEvento.classList.add('texto-evento');
 //         eventoHeader.appendChild(textoEvento);
-    
+
 //         // Verifica si data.f20_Detalles_Evento es un objeto
 //         if (typeof data.f20_Detalles_Evento === 'object' && data.f20_Detalles_Evento !== null) {
 //             const detalleEventoContainer = document.createElement('div'); // Crear un contenedor para los detalles del evento
 //             detalleEventoContainer.classList.add('detalle-evento-container');
-    
+
 //             const eventoDetalle = document.createElement('ul');
 //             eventoDetalle.classList.add('detalle-evento');
 //             data.f20_Detalles_Evento.L.forEach(detalle => {
@@ -353,10 +393,10 @@ const fetchData = async () => {
 //         } else {
 //             console.error("data.f20_Detalles_Evento no es un objeto o es nulo.");
 //         }
-    
+
 //         // Agregar el elemento del evento al contenedor principal
 //         eventosContainer.appendChild(eventoDiv);
-//       // }    
+//       // }
 //     });
 
 //     // Resto del código para procesar y mostrar los resultados...
